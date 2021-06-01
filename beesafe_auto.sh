a51aa6e949d3682c19c58026703f7030aa55b7be
harun ulum <student.harun0813@gmail.com>
2021-06-01 20:13:02 +0700

    add modules

--- beesafe_auto.sh
+++ beesafe_auto.sh
@@ -3,6 +3,7 @@
 source ./venv/bin/activate
 pip install -r mod.txt
 gdown https://drive.google.com/uc?id=1lphpSnzlD3zAtQNpKu-91vKdXRcALGAX
+mkdir models
 mv models.zip models
 unzip models/models.zip
 docker-compose up --build -d
